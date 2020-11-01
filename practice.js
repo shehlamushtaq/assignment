4701064e45e1d819081a1335985d1933f9d051bb
Shehla Mushtaq <shehla.asim.78@gmail.com>
2020-11-02 00:26:18 +0500

    work of arrey

--- practice.js
+++ practice.js
@@ -62,30 +62,220 @@
 //=============================op operators==========================
 
 
-var a , b, result;
+// var a , b, result;
 
-a = 2
-b = 1
-document.write(` value of a =  ${a} 
-value of a =  ${b}`);
+// a = 2
+// b = 1
+// document.write(` value of a =  ${a} 
+// value of a =  ${b}`);
 
-result = --a - --b + ++b + b--; 
-document.write(`complete result =  ${result}
-First Step = --a; = ${--a}
-second step = --a - --b = ${--a - --b}`)
+// result = --a - --b + ++b + b--; 
+// document.write(`complete result =  ${result}
+// First Step = --a; = ${--a}
+// second step = --a - --b = ${--a - --b}`)
+//=======================================================
+
+var country = ["Afghanistan","Albania","Algeria","Andorra","Angola",
+"Anguilla","Antigua &amp; Barbuda","Argentina","Armenia","Aruba",
+"Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh",
+"Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan",
+"Bolivia","Bosnia &amp; Herzegovina","Botswana","Brazil",
+"British Virgin Islands","Brunei","Bulgaria","Burkina Faso",
+"Burundi","Cambodia","Cameroon","Cape Verde","Cayman Islands",
+"Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica",
+"Cote D Ivoire","Croatia","Cruise Ship","Cuba","Cyprus","Czech Republic",
+"Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt",
+"El Salvador","Equatorial Guinea","Estonia","Ethiopia","Falkland Islands",
+"Faroe Islands","Fiji","Finland","France","French Polynesia",
+"French West Indies","Gabon","Gambia","Georgia","Germany","Ghana",
+"Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey",
+"Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary",
+"Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man",
+"Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya",
+"Kuwait","Kyrgyz Republic","Laos","Latvia","Lebanon","Lesotho","Liberia",
+"Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia",
+"Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Mauritania",
+"Mauritius","Mexico","Moldova","Monaco","Mongolia","Montenegro",
+"Montserrat","Morocco","Mozambique","Namibia","Nepal","Netherlands",
+"Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger"
+,"Nigeria","Norway","Oman","Pakistan","Palestine","Panama",
+"Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal",
+"Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda",
+"Saint Pierre &amp; Miquelon","Samoa","San Marino","Satellite",
+"Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore",
+"Slovakia","Slovenia","South Africa","South Korea","Spain","Sri Lanka",
+"St Kitts &amp; Nevis","St Lucia","St Vincent","St. Lucia","Sudan",
+"Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan",
+"Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad &amp; Tobago",
+"Tunisia","Turkey","Turkmenistan","Turks &amp; Caicos","Uganda","Ukraine",
+"United Arab Emirates","United Kingdom","Uruguay","Uzbekistan","Venezuela"
+,"Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];   
+//==========================find the index number of==========================
+// var task = "pakistan";
+// //  document.write(country.indexOf(task))
+// // var found = false;
+// for (var i = 0; i<=country.length; i++ ) {
+
+//     if (country[i].toLowerCase() == task) {
+//         document.write(i);
+//         break
+//     }
+// var ind = findIndex(country , "india")
+// document.write(ind)
+    
+
+
+// function findIndex(arr , name){
+  
+//     for (var i = 0; i<arr.length; i++ ) {
+
+//         if (arr[i].toLowerCase() == name) {
+//             return i
+//         }                
+//     }
+//     return -1
+
+// }
+//================find in arrey the elements which Start With ==============================
+
+// document.write(findList(country , "h"))
+
+// function findList (arr , lat) {
+//     var temp = [];
+//     var countryStr;
+    
+//     for ( var i = 0; i<arr.length; i++){
+//     countryStr = arr[i]
+//         if ( countryStr.charAt(0).toLowerCase() == lat.toLowerCase()) {
+//             temp.push(countryStr)
+//         }
+//     }
+//     return temp;
+// }
+//==============================find the elements that have tan in its speling========
+
+// document.write(findTan(country , "b"))
+
+
+// function findTan ( arr , tan) {
+//     var temp = [];
+//     var countryStr;
+//     for ( var i = 0; i<arr.length; i++) {
+//         countryStr = arr[i]
+//         for ( var j = 0 ; j<countryStr.length; j++) {
+//             if ( countryStr.charAt(j).toLowerCase() == tan.toLowerCase() ) {
+//                 temp.push (countryStr)
+//                 break
+//             }    
+//         }
+   
+//     } 
+//     return temp
+//}
+//======to do=========================================================================
+
+// var courses = [];
+
+// for (var i = 0; i<5; i++) {
+//     courses [i] = prompt(`enter the name of course ${i+1} ` )
+    
+// }
+// alert( ` Names of All the courses you have Entered
+// ` + courses)
+
+// var editCourse = []
+
+// for (var i = 0; i<5; i++) {
+//     editCourse[i] =  prompt(`Name of course ${i+1} = ${courses [i]}
+//     Edit the course name if you want , other wise press Enter `, courses[i] )
+  
+// }
+//  alert( `All The Names of your Selected courses  
+//  `+ editCourse)
+//=======================for multi charechtor searching===================
+// document.write(multiChar(country , 'tan'))
+
+// function multiChar (arr , lat) {
+//     var temp = [] 
+//     var str;
+//     for ( var i = 0; i< arr.length; i++) {
+//         str = arr[i]
+//         //for (var j = 0; j<str.length; j++){
+            
+//         var newStr = str.indexOf( lat)
+//             // if (  lat === (str.slice(newStr , lat.length) )) {
+//         if ( newStr != -1) {
+//             temp.push(str)
+//         //    break
+//         }
+//         //} 
+//     }
+//     return temp
+// }
 
 
 
 
 
+// console.log(courses)
 
 
 
+var p1 = `Asim  is the Elder son of Iqtidar Ahmed man. Asim is a good man. Asim is the Elder son of Iqtidar Ahmed. Asim is a good man. Asim is the Elder son of Iqtidar Ahmed. Asim is a good man. Asim is the Elder son of Iqtidar Ahmed. Asim is a good man. Asim is the Elder son of Iqtidar Ahmed. Asim is a good man.`;
+document.write(findNameCount(p1 , "man"));
 
 
+// function findNameCount (str , name){
+//     var a =str.split(' ' );
+//     //console.log(a);
+//     var count = 0;
+//     for (var i = 0; i<a.length; i++){
+
+//       if (a[i] == name ){
+//           //count = ++count;
+//           count++;
+//       }   
+//     }
+//     return count
+// }    
+
+// function findNameCount (str , name) {
+//     var count = 0;
+//     //for ( var i = 0; i<str.length; i++){
+//         var temp = str.match(/asim/g)
+
+//         if (temp) {
+//             count = temp.length;
+//         } 
+//     //}
+//     return count
+// }
+
+function findNameCount (str, name) {
+
+    var count = 0;
+    var start = 0;
+    var searchItemLength = name.length;
+
+    var ind;
+
+    while (true) {
+
+        ind = str.indexOf(name, start);
+
+        if (ind != -1) {
+            count++;
+            start = ind + searchItemLength;
+        } else {
+            break;
+        }
+
+    }
+
+    return count;
 
 
-
+}
 
 
 
